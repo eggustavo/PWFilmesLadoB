ba49bb2d7915f280180a6bd1b8866011bfbeab0b
Aluno <aluno@etec.sp.gov.br>
2023-09-25 20:47:22 -0300

    Atividades Aula - 25/09/2023

--- PWFilmes.Domain/Categoria.cs
+++ PWFilmes.Domain/Categoria.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PWFilmes.Domain
 {
+    [Table("Categoria")]
     public class Categoria
     {
+        [Key]
+        [Required(ErrorMessage = "O Código deve ser informado.")]
         public int Codigo { get; set; }
+
+        [Required(ErrorMessage = "A Descrição deve ser informada.")]
+        [MaxLength(100, ErrorMessage = "A Descrição deve conter no máximo 100 caracteres.")]
         public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "A Cor deve ser informada.")]
+        [MaxLength(50, ErrorMessage = "A Cor deve conter no máximo 50 caracteres.")]
         public string Cor { get; set; }
     }
 }
